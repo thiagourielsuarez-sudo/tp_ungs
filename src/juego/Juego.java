da6ae8759573b243580ad5cf067d8f5d5f70ec1a
Thiago Suarez <thiagourielsuarez@gmail.com>
2026-06-02 21:29:10 -0300

    actualizacion

---
+++ src/juego/Juego.java
@@ -0,0 +1,162 @@
+package juego;
+
+
+import java.awt.Color;
+
+import entorno.Entorno;
+import entorno.Herramientas;
+import entorno.InterfaceJuego;
+import java.awt.Image;
+
+public class Juego extends InterfaceJuego
+{
+
+	private Entorno entorno;  
+	private Plataforma[] plataformas;
+	private Enemigo[] enemigos;
+	Image imagenfondo;
+	personaje p = new personaje();
+	fondo f = new fondo();
+	castillo c = new castillo();
+	
+		
+	public Juego() {
+		
+		enemigos = new Enemigo[20];
+		
+        imagenfondo = Herramientas.cargarImagen("fondoF.png");
+     
+        int[] alturas = {200, 350, 500};
+        int[]anchos = {120,160,200};
+        plataformas = new Plataforma[30];
+        plataformas[0] = new Plataforma(400, 300, 160, 30);
+        plataformas [1] = new Plataforma(3700, 600, 600, 40);
+        for (int i = 2; i < plataformas.length; i++) {
+            int x = 400 + i * 100; 
+            int y = alturas[(int)(Math.random() * alturas.length)];
+            int anchopla = anchos[(int)(Math.random() * anchos.length)];
+            plataformas[i] = new Plataforma(x, y, anchopla, 30);
+            
+        }             
+        this.entorno = new Entorno(this, "Proyecto para TP", 800, 600);
+        this.entorno.iniciar();
+    }
+	
+	private void generarEnemigo() {
+		 int[] alturas = {150, 300, 450};
+	    for (int i = 0; i < enemigos.length; i++) {
+
+	        if (enemigos[i] == null) {
+	        	int yy =	alturas[(int)(Math.random() * alturas.length)];
+	            if (Math.random() < 0.5) {
+	                enemigos[i] = new Enemigo( -20, yy ,2);
+
+	            } else {enemigos[i] = new Enemigo(820, yy ,-2);
+	            }
+
+	            return;
+	        }
+	    }
+	}
+
+	public void tick() {
+		
+		
+		int vivos = 0;
+	
+
+		for (int i = 0; i < enemigos.length; i++) {
+
+		    if (enemigos[i] != null) {
+		        vivos++;
+		    }
+		}
+
+		while (vivos < 6) {
+		    generarEnemigo();
+		    vivos++;
+		}
+        this.entorno.dibujarImagen(imagenfondo, f.x, f.y, 0, f.escala);
+    
+        double velocidadScroll = 0;
+        if (!c.gano && entorno.estaPresionada('d')) {
+            velocidadScroll = -3.0; 
+            f.movimientofondo(entorno); 
+        }
+
+      
+        int[] alturas = {200, 350, 500};
+        int[]ancho = {120,160,200};
+        for (int i = 0; i < plataformas.length; i++) {
+            if (plataformas[i] != null) {
+                plataformas[i].mover(velocidadScroll);
+                plataformas[i].dibujar(entorno);
+                if (plataformas[i].derecha() < 0) {
+                    plataformas[i] = null;
+                }
+            }
+        } 
+        for (int i = 0; i < enemigos.length; i++) {
+
+            if (enemigos[i] != null) {
+
+                enemigos[i].mover();
+
+                if (entorno.estaPresionada('d')) {
+                    enemigos[i].moverConScroll(-2.0);
+                }
+
+                enemigos[i].dibujar(entorno);
+
+                if (enemigos[i].colisiona(p)) {
+                    p.recibirDanio(1);
+                    enemigos[i] = null;
+                }
+
+                if (enemigos[i] != null && enemigos[i].fueraDePantalla()) {
+                    enemigos[i] = null;
+                }
+            }
+        }
+        
+        c.mover(velocidadScroll);
+        c.dibujar(entorno);
+
+       
+        if (c.tocarCastillo(p) && !c.gano()) {
+            c.setGano(true);
+        }
+
+       
+        if (c.gano()) {
+            velocidadScroll = 0;
+            p.velocidad=0;
+            
+          
+            entorno.cambiarFont("Arial", 100, Color.YELLOW);
+            entorno.escribirTexto("¡GANASTE!", 150, 200);
+        }
+
+        p.Movimiento(entorno, plataformas);
+        p.Fotogramader(entorno);
+        p.Fotogramaizq(entorno);
+        p.limite(entorno);
+     
+        if (p.ultimadireccion.equals("derecha")) {
+            this.entorno.dibujarImagen(p.Fotos[p.i], p.x, p.y, 0, 0.4);
+        } else {
+            this.entorno.dibujarImagen(p.imagenes[p.k], p.x, p.y, 0, 0.4);
+        }
+        entorno.cambiarFont("Arial", 20, Color.WHITE);
+        entorno.escribirTexto("Vida: " + p.getVida(), 20, 30);
+       
+       
+        
+    }
+	@SuppressWarnings("unused")
+	public static void main(String[] args)
+	{
+		Juego juego = new Juego();
+	}
+
+}
---
+++ src/juego/personaje.java
@@ -0,0 +1,190 @@
+package juego;
+
+import java.awt.Color;
+import entorno.Entorno;
+import entorno.Herramientas;
+import java.awt.Image;
+
+public class personaje {
+	
+	int x = 400;
+	int y = 100;
+	int velocidad = 3;
+	int vida = 3;
+	Image foto1 = Herramientas.cargarImagen("foto1.png");
+	Image foto2 = Herramientas.cargarImagen("foto2.png");
+	Image foto3 = Herramientas.cargarImagen("foto3.png");
+	Image foto4 = Herramientas.cargarImagen("foto4.png");
+	Image foto5 = Herramientas.cargarImagen("foto5.png");
+	Image foto6 = Herramientas.cargarImagen("foto6.png"); 
+	Image foto7 = Herramientas.cargarImagen("foto7.png");
+	Image foto8 = Herramientas.cargarImagen("foto8.png");
+	Image foto9 = Herramientas.cargarImagen("foto9.png");   
+	
+	Image imagen1 = Herramientas.cargarImagen("image1.png");
+	Image imagen2 = Herramientas.cargarImagen("image2.png");
+	Image imagen3 = Herramientas.cargarImagen("image3.png");
+	Image imagen4 = Herramientas.cargarImagen("image4.png");
+	Image imagen5 = Herramientas.cargarImagen("image5.png");
+	Image imagen6 = Herramientas.cargarImagen("image6.png"); 
+	Image imagen7 = Herramientas.cargarImagen("image7.png");
+	Image imagen8 = Herramientas.cargarImagen("image8.png");
+	Image imagen9 = Herramientas.cargarImagen("image9.png");   
+	
+	Image [] imagenes = {imagen1, imagen2, imagen3, imagen4, imagen5, imagen6, imagen7, imagen8, imagen9};
+	Image [] Fotos  =  {foto1, foto2, foto3, foto4, foto5, foto6, foto7, foto8, foto9,}; 
+	String ultimadireccion = "derecha";
+	int contadorticks = 0;
+	int contadortick = 0;
+	int i = 0;
+	int k = 0;
+	double velocidadY;
+	castillo g = new castillo();
+	
+	public void Movimiento(Entorno entorno, Plataforma[] plataformas) {  
+		for (int j = 0; j < plataformas.length; j++) {
+
+		    if (plataformas[j] != null) {
+
+		        if (entorno.estaPresionada('d') && chocaIzquierda(plataformas[j])) {
+		            x = (int) plataformas[j].izquierda() - 15;
+		        }
+
+		        if (entorno.estaPresionada('a') && chocaDerecha(plataformas[j])) {
+		            x = (int) plataformas[j].derecha() + 15;
+		        }
+		    }
+		}
+
+	
+		// Solo mover a la derecha si NO ganó
+		if (!g.gano && entorno.estaPresionada('d') && x < 700) {
+		    x += velocidad;
+		    ultimadireccion = "derecha";
+		}
+	    if (entorno.estaPresionada('a')) { x -= velocidad; ultimadireccion = "izquierda"; }    
+	    
+	  
+	    if (estaApoyada(plataformas)) {
+	    
+	        velocidadY = 0; 
+	        
+	      
+	    if (entorno.sePresiono('w')) {
+	            velocidadY = -8;
+	        }
+	    } else {
+	        
+	        velocidadY += 0.2; 
+	    }		if (velocidadY >8)
+	    			velocidadY = 8;
+	    
+	   
+	    y += velocidadY;
+	}
+
+	public void Fotogramader (Entorno entorno) {
+		if ((entorno.estaPresionada('d')) == true) {
+			contadorticks +=1;
+			if (contadorticks == 8)    {
+				i+=1; contadorticks = 0;
+				if (i >= Fotos.length-1)   { i = 0 ; }
+			}
+		} else {
+			if (entorno.estaPresionada('a')== false) {
+				contadorticks = 0;   
+				if (ultimadireccion.equals("derecha")) { i = 8; }   
+			}
+		}		
+	}			
+
+	public void Fotogramaizq (Entorno entorno) {
+		if ((entorno.estaPresionada('a')) == true) {
+			contadortick +=1;
+			if (contadortick == 8)    {
+				k+=1; contadortick = 0;
+				if (k >= imagenes.length-1)   { k = 0 ; }
+			}
+		} else {
+			if (entorno.estaPresionada('d')==false) {
+				contadortick = 0;
+				if (ultimadireccion.equals("izquierda")) { k = 8; }		
+			}						
+		}
+	}
+
+	public void limite (Entorno entorno) {
+		if (x > 700) x = 700;
+		if (x < 20) x = 20;
+		if (y > 550) y= 50;
+		if (y < 50) y = 50;
+	}
+	
+	public boolean estaApoyada (Plataforma[] plataformas) {
+
+	    for (int j = 0; j < plataformas.length; j++) {
+
+	        if (plataformas[j] != null) {
+
+	            if (chocaArriba(plataformas[j])) {
+	                y = (int) plataformas[j].arriba() - 35;
+	                velocidadY = 0;
+	                return true;
+	            }
+
+	            if (chocaAbajo(plataformas[j])) {
+	                y = (int) plataformas[j].abajo() + 35;
+	                velocidadY = 0;
+	            }
+	        }
+	    }
+	 return false;
+	}
+	private boolean chocaArriba(Plataforma p) {
+	    return (
+	        this.x > p.izquierda() &&
+	        this.x < p.derecha() &&
+	        this.y + 35 >= p.arriba() &&
+	        this.y + 35 <= p.arriba() + 10 &&
+	        velocidadY >= 0
+	    );
+	}
+
+	private boolean chocaAbajo(Plataforma p) {
+	    return (
+	        this.x > p.izquierda() &&
+	        this.x < p.derecha() &&
+	        this.y - 35 <= p.abajo() &&
+	        this.y - 35 >= p.abajo() - 10 &&
+	        velocidadY < 0
+	    );
+	}
+	private boolean chocaIzquierda(Plataforma p) {
+	    return (
+	        this.x + 15 >= p.izquierda() &&
+	        this.x < p.izquierda() &&
+	        this.y + 30 > p.arriba() &&
+	        this.y - 30 < p.abajo()
+	    );
+	}
+
+	private boolean chocaDerecha(Plataforma p) {
+	    return (
+	        this.x - 15 <= p.derecha() &&
+	        this.x > p.derecha() &&
+	        this.y + 30 > p.arriba() &&
+	        this.y - 30 < p.abajo()
+	    );
+	}
+	
+	
+	
+	public void recibirDanio(int cantidad) {
+		this.vida -= cantidad;
+	}
+	
+	public int getVida() {
+		return this.vida;
+	}
+	
+} 
